0d3c8fd7ed6f2860a4d853d0b34b51ecfc7dc1c2
varunborar <varunborar@gmail.com>
2020-10-01 15:30:54 +0530

    Let the light prevail

--- ytdownload/__main__.py
+++ ytdownload/__main__.py
@@ -3,11 +3,10 @@ import sys
 
 def main():
     print('in main')
-    args = sys.argv[1]
     # print('count of args :: {}'.format(len(args)))
     # for arg in args:
     #     print('passed argument :: {}'.format(arg))
-
+    args = input("Link :")
     ydl_opts = {} 
   
     def dwl_vid(): 
